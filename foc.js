0d45df1ae46df0b844cccf6cb6ef9255be81c5fe
johnjbarton <johnjbarton@johnjbarton.com>
2011-08-04 16:22:52 -0700

    TDD shell

---
+++ foc.js
@@ -0,0 +1,7 @@
+// See license.txt, BSD
+// Copyright 2011 johnjbarton@google.com
+
+/*
+ * Implementation of Function-object Creation algorithm for Naming Anonymous JavaScript Functions.
+ *  
+ */
