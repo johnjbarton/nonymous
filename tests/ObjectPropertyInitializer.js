dd05c7f9fcfcfb2761ed4bdecb21e946167ccf38
johnjbarton <johnjbarton@johnjbarton.com>
2011-08-04 16:23:33 -0700

    read and parse test case on load

---
+++ tests/ObjectPropertyInitializer.js
@@ -0,0 +1,11 @@
+// see ../license.txt, BSD
+
+// Case 1: Object Property Initializer
+
+var anObject = {
+  aPropertyInitialized: function() {
+    return true;
+  }
+};
+
+anObject.aPropertyInitialized.displayName = "anObject.aPropertyInitialized";
