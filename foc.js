d2abc0dd118789fee33bd2d7b89cac4711bbb832
johnjbarton <johnjbarton@johnjbarton.com>
2011-08-08 14:21:01 -0700

    start test case driver

--- foc.js
+++ foc.js
@@ -2,6 +2,14 @@
 // Copyright 2011 johnjbarton@google.com
 
 /*
- * Implementation of Function-object Creation algorithm for Naming Anonymous JavaScript Functions.
+ * Implementation of Function-object Consumption algorithm for Naming Anonymous JavaScript Functions.
  *  
  */
+var uglifoc = {
+  foc: function(node, next) {
+    if (node.type ==="function") {
+      console.log("ulgifoc.foc "+node.type+" has parent:"+(node.parent?node.parent.type:"none"), node);
+    }
+    return next();
+  }
+};
